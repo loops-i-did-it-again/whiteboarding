2d888f9fa2925a197a9eabc8cc85e31f3ee3cdb4
dani <dani.zaghian@gmail.com>
2020-09-25 13:48:51 -0700

    adds whiteboarding w04

---
+++ practice_w04.rb
@@ -0,0 +1,20 @@
+# Write a function that takes in two strings and returns the longest common substring.
+
+# example input: "marathon" "parachute", "together", "bothersome"
+# expected output: "ara", "ther"
+
+# Write a function that takes in two strings and prints out each one.
+# Write a function that takes in two strings and prints out the letter of each string one at a time.
+# Write a function that takes in two strings and loops through the letters of the first string and then compares the letter to each letter of the second string (print "match" if  they match).
+
+
+
+# Write a function that takes in an array of numbers and returns a new array of the same numbers without any duplicates.
+
+# example input = [3, 4, 7, 1, 9, 1, 3]
+# expected output [3, 4, 7, 1, 9]
+
+# Write a function that takes in an array of numbers and prints it.
+# Write a function that accepts and array of numbers and loops through each number and print it out (separate line).
+# Write a function that compares the first number in the array to each other number in the array and prints "GRATS" if they match.
+# Write a function that takes in an array of numbers and loops through each number and runs a second inner loop that compares the current number with each other number (print "GRATS" if they match).
