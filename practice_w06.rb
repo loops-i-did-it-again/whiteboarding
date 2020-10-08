426b5f2a4b75deefceae4560153dcf31d0cdb2b4
dani <dani.zaghian@gmail.com>
2020-10-08 13:49:49 -0700

    adds whiteboarding decomposition

---
+++ practice_w06.rb
@@ -0,0 +1,10 @@
+# Write a function that takes in a string of code with parentheses, square brackets, and curly brackets, and checks if the code is balanced with correct bracket syntax.
+
+# example input: "{x + ([0], a: 2}"
+# false
+# example input: "(x + [1, {a:2, b: 3}])"
+# true
+
+# Write a function that takes in a string and prints out the characters of the string one line at a time
+# Write a function that takes in a string and counts how many open parenthesis and close parenthesis there are in the string
+# Write a function that takes in a string and counts how many open parenthesis, curly brackets, and square brackets there are, as well as close parenthesis, curly brackets, and square brackets there are.
